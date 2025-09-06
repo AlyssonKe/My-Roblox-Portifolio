53bdc594eac8fbb2f65d4e7236f8c869069019e5
Duelan <alyssonkepm87@gmail.com>
2025-09-06 03:39:12 -0300

    New UBG contribution and other game descriptions fixed

--- src/data/games.ts
+++ src/data/games.ts
@@ -8,7 +8,7 @@ export const games = [
     link: "https://www.roblox.com/games/13772394625/Blade-Ball",
     title: "Blade Ball",
     description:
-      "I'm currently working on Blade Ball, a multiplayer PvP game where players compete to see who’s the best at deflecting a ball using a sword. The game has already surpassed <b>5 BILLION visits</b> and consistently has over <b>20,000 concurrent players</b> every day!",
+      "I'm currently working on Blade Ball, a multiplayer PvP game where players compete to see who's the best at deflecting a ball using a sword. The game has already surpassed <b>5 BILLION visits</b> and consistently has over <b>20,000 concurrent players</b> every day!",
   },
   {
     id: "FISCH",
@@ -20,13 +20,23 @@ export const games = [
     description:
       "Fisch was the first game I had the opportunity to work on that wasn't created by me. It was also my first experience being part of a large and professional team. I learned a lot during my time developing there and gained valuable experience. I had the pleasure of contributing directly to major events such as <b><i>Roblox Mega Hunt 2025</i></b> and <b><i>Jurassic World Rebirth</i></b>. Today, Fisch has over <b>3 BILLION visits</b> and more than <b>20,000 concurrent players</b> every day.",
   },
+    {
+    id: "UBG",
+    universeId: 8478387499,
+    contribuition: true,
+    image: "/images/game-thumbnails/UBG.png",
+    link: "https://www.roblox.com/games/136830569531877/untitled-boss-game",
+    title: "untitled boss game",
+    description:
+      "Developing Untitled Boss Game was a completely different experience for me. I had a very short timeframe of <b>just two weeks</b>, which made the process a huge challenge. The game revolves around defeating bosses to earn cash, buy items, and grow stronger. During this time, I was fully responsible for designing and programming all the game's systems and mechanics, which gave me valuable and intense learning experience.",
+  },
   {
     id: "TT",
     image: "/images/game-thumbnails/TT.png",
     link: "https://www.roblox.com/games/4237861040/Treacherous-Tower",
     title: "Treacherous Tower",
     description:
-      "Treacherous Tower was my first big success. In it, players climb a tower full of obstacles that change every 60 seconds, using power-ups to help or hinder others...",
+      "Treacherous Tower was my first big success. In it, players climb a tower full of obstacles that change every 60 seconds, using power-ups to help or hinder others. Released with no expectations, it was surprisingly successful, reaching almost 30 thousand simultaneous players. As the main developer, I created everything from scratch, including systems like tower generation, power-ups, shop, missions, leaderboard, and more.",
   },
   {
     id: "ECT",
@@ -34,7 +44,7 @@ export const games = [
     link: "https://www.roblox.com/games/118073417231051/Elemental-Castle-Tycoon",
     title: "Elemental Castle Tycoon",
     description:
-      "Build a magnificent castle and acquire incredible and unique powers to fight against your enemies...",
+      "Build a magnificent castle and acquire incredible and unique powers to fight against your enemies. Dominate the server and become invincible. This was a solo project I created during my first semester break in college. In a short period of time, I was able to apply the knowledge I had learned and improve various skills. I was responsible for all the building, modeling, programming, and visual effects in the game. It was such an enriching experience since it had been a while since I last created something on my own.",
   },
   {
     id: "PPT",
@@ -42,7 +52,7 @@ export const games = [
     link: "https://www.roblox.com/games/16497788573/Pink-Palace-Tycoon",
     title: "Pink Palace Tycoon",
     description:
-      "In Pink Palace Tycoon, you build the palace of your dreams while facing fun challenges and minigames...",
+      "In Pink Palace Tycoon, you build the palace of your dreams while facing fun challenges and minigames. Explore a magical world, relax in your pool, sunbathe on the beach, and dress up with style. This was my first project with a team of 6 people, each person having a specific task. I was responsible for the main programming, creating systems such as avatar customization, tycoon progression and saving, missions, minigames, interactions, shop, NPCs, rewards, and more. Additionally, I was in charge of coordinating the team at the beginning of the project.",
   },
   {
     id: "John-And-Mark",
@@ -50,6 +60,6 @@ export const games = [
     link: "https://www.roblox.com/games/14108196267/The-Adventures-of-John-and-Mark",
     title: "John And Mark",
     description:
-      `"The Adventures of John and Mark" is a game that narrates the story of a character who moves to a new house and finds a cartoon tape...`,
+      `"The Adventures of John and Mark" is a game that narrates the story of a character who moves to a new house and finds a cartoon tape starring John and his brother Mark. What seems like just a peculiar cartoon soon reveals itself as a mystery, leading to the discovery of a disturbing past about the brothers. This project was a collaboration with my best friend, who was responsible for most of the visual assets and modeling, while I developed all the systems of the game, including shop, lobby match, dialogues and choices, inventory, animations, and more.`,
   },
 ];
